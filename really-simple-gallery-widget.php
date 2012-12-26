304d575c7d89732350df8ead00cff6dd8627df47
helen <helen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-26 14:28:05 +0000

    Massive update to plugin - see 1.2 changelog for more details
    git-svn-id: https://plugins.svn.wordpress.org/really-simple-gallery-widget/trunk@644634 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- really-simple-gallery-widget.php
+++ really-simple-gallery-widget.php
@@ -1,82 +1,91 @@
 <?php
 /*
 Plugin Name: Really Simple Gallery Widget
-Plugin URI: http://www.helenhousandi.com/wordpress/plugins/really-simple-gallery-widget/
+Plugin URI: http://helen.wordpress.com/category/plugins/really-simple-gallery-widget/
 Description: Widget to display images attached to a specific page/post or from the entire media library. Includes options for number of images, display order (including true random), any registered image size, caption toggling, type of link, rel attribute, and link title prefix. Can also show images attached to the page/post currently being viewed.
-Version: 1.1
+Version: 1.2
 Author: Helen Hou-Sandi
-Author URI: http://www.helenhousandi.com
+Author URI: http://helenhousandi.com
 */
 
-// function for proper associative array shuffling
-if ( !function_exists('shuffle_assoc') ) {
-	function shuffle_assoc($list) {
-	  if (!is_array($list)) return $list;
-	
-	  $keys = array_keys($list);
-	  shuffle($keys);
-	  $random = array();
-	  foreach ($keys as $key)
-	    $random[$key] = $list[$key];
+if ( ! class_exists( 'RSGWidget' ) ) {
 
-	  return $random;
-	}
-}
-
-if ( !class_exists('RSGWidget') ) {
 	class RSGWidget extends WP_Widget {
-	
-	  function RSGWidget() {
-	    $widget_ops = array('classname' => 'widget_rsg', 'description' => __('Grab photos from a specified post/page or the entire media library and display them in a widget area.'));
-	    $this->WP_Widget('RSGWidget', __('Really Simple Gallery Widget'), $widget_ops);
-	  }
-		
-	  function widget( $args, $instance ) {
-	    extract($args);
-	    $title = apply_filters('widget_title', empty($instance['title']) ? '' : esc_html($instance['title']), $instance);
-	    $post_id = $instance['post_id'];
-	    $full_library = $instance['full_library'] ? '1' : '0';
-	    $current_post_images = $instance['current_post_images'] ? '1' : '0';
-	    $num_images = $instance['num_images'];
-	    $show_captions = $instance['show_captions'] ? '1' : '0';
-	    $image_size = $instance['image_size'];
-	    $order = $instance['order'];
-	    $link_type = $instance['link_type'];
-	    $before_link_title = esc_attr( strip_tags($instance['before_link_title']) );
-	    $link_rel = esc_attr( strip_tags($instance['link_rel']) );
-	    
-	    include('views/widget.php');
-	  }
-	  
-	  function update( $new_instance, $old_instance ) {
+		function RSGWidget() {
+			$widget_ops = array(
+				'classname' => 'widget_rsg',
+				'description' => __('Grab photos from a specified post/page or the entire media library and display them in a widget area.', 'rsg-widget' )
+			);
+
+			$this->WP_Widget( 'RSGWidget', __('Really Simple Gallery Widget', 'rsg-widget' ), $widget_ops );
+
+			add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+		}
+
+		function widget( $args, $instance ) {
+			$args['title'] = apply_filters( 'widget_title', $instance['title'] );
+			$args['from'] = $instance['from'];
+			$args['post_id'] = isset( $instance['post_id'] ) ? absint( $instance['post_id'] ) : false;
+			$args['att_ids'] = isset( $instance['att_ids'] ) ? $instance['att_ids'] : false;
+			$args['current_post_images'] = $instance['current_post_images'] ? true : false;
+			$args['num_images'] = $instance['num_images'];
+			$args['show_captions'] = $instance['show_captions'] ? true : false;
+			$args['image_size'] = $instance['image_size'];
+			$args['order'] = $instance['order'];
+			$args['orderby'] = $instance['orderby'];
+			$args['link_type'] = $instance['link_type'];
+			$args['before_link_title'] = esc_attr( strip_tags($instance['before_link_title']) );
+			$args['link_rel'] = esc_attr( strip_tags( $instance['link_rel'] ) );
+
+			// back compat for old full_library option
+			if ( isset( $instance['full_library'] ) )
+				$args['from'] = 'full_library';
+
+			include( 'views/widget.php' );
+		}
+
+		function update( $new_instance, $old_instance ) {
 			$instance = $old_instance;
-			$instance['title'] = esc_html( strip_tags($new_instance['title']) );
-			if ( !ctype_digit($new_instance['post_id']) )
-			  $instance['post_id'] =  '';
-			else
-			  $instance['post_id'] = intval($new_instance['post_id']);
-			$instance['full_library'] = $new_instance['full_library'] ? 1 : 0;
+			$instance['title'] = wp_kses_post( $new_instance['title'] );
+			$instance['from'] = sanitize_key( $new_instance['from'] );
+			$instance['post_id'] = absint( $new_instance['post_id'] );
+
+			// don't want to store a 0
+			if ( 0 === $instance['post_id'] )
+				$instance['post_id'] = '';
+
+			$instance['att_ids'] = wp_kses( $new_instance['att_ids'] );
 			$instance['current_post_images'] = $new_instance['current_post_images'] ? 1 : 0;
-		  if ( !ctype_digit($new_instance['num_images']) )
-		    $instance['num_images'] =  '0';
-		  else
-		    $instance['num_images'] = intval($new_instance['num_images']);
+			$instance['num_images'] = absint( $new_instance['num_images'] );
 			$instance['show_captions'] = $new_instance['show_captions'] ? 1 : 0;
-			$instance['image_size'] = $new_instance['image_size'];
-			$instance['order'] = $new_instance['order'];
-			$instance['link_type'] = $new_instance['link_type'];
-			$instance['before_link_title'] = esc_attr( strip_tags($new_instance['before_link_title']) );
-			$instance['link_rel'] = esc_attr( strip_tags($new_instance['link_rel']) );
+			$instance['image_size'] = sanitize_key( $new_instance['image_size'] );
+			$instance['order'] = sanitize_key( $new_instance['order'] );
+			$instance['orderby'] = sanitize_key( $new_instance['orderby'] );
+			$instance['link_type'] = sanitize_key( $new_instance['link_type'] );
+			$instance['before_link_title'] = esc_html( $new_instance['before_link_title'] );
+			$instance['link_rel'] = esc_html( $new_instance['link_rel'] );
+
+			// remove old full_library option, if it exists
+			if ( isset( $instance['full_library'] ) )
+				unset( $instance['full_library'] );
+
 			return $instance;
-	  }
-	
-	  function form( $instance ) {
-			include('views/form.php');
+		}
+
+		function form( $instance ) {
+			include( 'views/form.php' );
+		}
+
+		public function admin_enqueue_scripts($hook) {
+			if ( ! 'widgets.php' === $hook )
+				return;
+
+			wp_enqueue_script( 'rsgwidget', plugins_url( 'js/rsgwidget.js', __FILE__ ), array( 'jquery' ), '1.2', true );
 		}
 	}
-	function RSGWidget_init() {
-	    register_widget('RSGWidget');
+
+	add_action( 'widgets_init', 'rsgwidget_init' );
+	function rsgwidget_init() {
+		register_widget( 'RSGWidget' );
 	}
-	
-	add_action('widgets_init', 'RSGWidget_init');
 }
